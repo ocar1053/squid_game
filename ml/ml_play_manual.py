7f59e1a4776f5c0023b40cdf833b1447ea967d53
Kylin_on_Mac <kylingithubdev@gmail.com>
2024-09-25 16:05:07 +0800

    feat:create food averagely

--- ml/ml_play_manual.py
+++ ml/ml_play_manual.py
@@ -14,7 +14,7 @@ class MLPlay:
         Generate the command according to the received scene information
         """
         # pprint("AI received data from game :", orjson.dumps(scene_info))
-        pprint(scene_info)
+        # pprint(scene_info)
         actions = []
 
         if pygame.K_w in keyboard or pygame.K_UP in keyboard:
--- src/game.py
+++ src/game.py
@@ -10,7 +10,7 @@ from mlgame.view.audio_model import create_music_init_data, create_sound_init_da
 from mlgame.view.decorator import check_game_progress, check_game_result
 from mlgame.view.view_model import *
 from .foods import *
-from .game_object import Squid, LevelParams, ScoreText
+from .game_object import Squid, LevelParams, ScoreText, WindowConfig
 
 
 def revise_squid_coordinate(squid: Squid, playground: pygame.Rect):
@@ -75,7 +75,14 @@ class SwimmingSquid(PaiaGame):
             self._score_to_pass = game_params.score_to_pass
             self._frame_limit = game_params.time_to_play
             self.playground.center = ((WIDTH - WIDTH_OF_INFO) / 2, HEIGHT / 2)
-
+            self._food_window = WindowConfig(
+                left=self.playground.left, right=self.playground.right,
+                top=self.playground.top, bottom=self.playground.bottom)
+            self._garbage_window = WindowConfig(
+                left=self.playground.left, right=self.playground.right,
+                top=self.playground.top - 60, bottom=self.playground.top - 10)
+            self._food_pos_list = []
+            self._garbage_pos_list = []
             # init game
             self.squid = Squid()
             self.foods.empty()
@@ -357,17 +364,46 @@ class SwimmingSquid(PaiaGame):
         for i in range(count):
             # add food to group
             food = FOOD_TYPE(self.foods)
-            if isinstance(food, Garbage):
+            if isinstance(food, (Food1, Food2, Food3,)):
+                # if food pos list is empty , re-create
+                if len(self._food_pos_list) < 1:
+                    self._food_pos_list = divide_window_into_grid(
+                        self._food_window)
+                pos = self._food_pos_list.pop()
                 food.set_center_x_and_y(
-                    random.randint(self.playground.left, self.playground.right),
-
-                    random.randint(self.playground.top - 30, self.playground.top)
-
+                    pos[0],
+                    pos[1]
                 )
-            else:
+
+
+            elif isinstance(food, (Garbage1, Garbage2, Garbage3,)):
+                if len(self._garbage_pos_list) < 1:
+                    self._garbage_pos_list = divide_window_into_grid(
+                        self._garbage_window, rows=1, cols=10)
+                pos = self._garbage_pos_list.pop()
                 food.set_center_x_and_y(
-                    random.randint(self.playground.left + 20, self.playground.right - 20),
-                    random.randint(self.playground.top + 20, self.playground.bottom - 20)
+                    pos[0],
+                    pos[1]
                 )
 
         pass
+
+
+def divide_window_into_grid(window: WindowConfig, rows: int = 10, cols: int = 10) -> list[(int, int)]:
+    grid_positions = []
+
+    # Calculate width and height of each grid piece
+    width = (window.right - window.left) // cols
+    height = (window.bottom - window.top) // rows
+
+    # Generate grid positions
+    for row in range(rows):
+        for col in range(cols):
+            center_x = window.left + col * width + width // 2
+            center_y = window.top + row * height + height // 2
+            grid_positions.append((center_x, center_y))
+
+    # Shuffle the list to randomize the order of positions
+    random.shuffle(grid_positions)
+
+    return grid_positions
